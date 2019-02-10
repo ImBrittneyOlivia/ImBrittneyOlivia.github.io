1fc7da190cdb6dae773aa6bd0683143c3ea81030
education.devs <education.devs@gmail.com>
2019-02-10 12:28:54 -0700

    added food project

---
+++ Food/script.js
@@ -0,0 +1,17 @@
+// $(document).ready(function() {
+//   $("#menu1").hide(function() {
+//     $(window).on("scroll", function() {
+//       $("#menu1").slideDown(4000);
+//     });
+//   });
+// });
+
+// $("#menu1")
+// .hide(2000)
+// .slideDown(1000)
+// .hide();
+
+// $(".menu2-acai")
+// .hide(7000)
+// .slideDown(1000)
+// .hide();
